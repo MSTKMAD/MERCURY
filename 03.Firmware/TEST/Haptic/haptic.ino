a41b8cf58cec0fa67c5d70e43255bb9a9144f8b6
Jmusotoku <javier@musotoku.com>
2022-10-04 10:33:08 +0200

    Test sensor haptico + Librerias asociada

---
+++ 03.Firmware/TEST/Haptic/haptic.ino
@@ -0,0 +1,20 @@
+/**
+ * @file haptic.ino
+ * @author Javi (Javier@musotoku.com)
+ * @brief 
+ * @version 1
+ * @date 2022-09-30
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+void setup()
+{
+    
+}
+
+void loop()
+{
+    
+}
